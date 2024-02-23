80d871c993f7842fde02b7a6e1a32564284ac365
FusionR1ot <156235870+FusionR1ot@users.noreply.github.com>
2024-02-23 12:52:21 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
-function position (stabber: Sprite) {
-    stabber.setKind(SpriteKind.Enemy)
+function enemy (myImage: Image) {
+    stabber = sprites.create(myImage, SpriteKind.Enemy)
+    return stabber
 }
+function spawn () {
+    let myImage: Image = null
+    enemy(myImage)
+}
+let stabber: Sprite = null
 game.splash("\"you can't do anything\"")
 game.splash("\"you're worthless\"")
-let mySprite = Render.getRenderSpriteVariable()
 game.splash("I'll prove them wrong ")
 tiles.setCurrentTilemap(tilemap`level`)
-let stabber = sprites.create(assets.image`stabby`, SpriteKind.Enemy)
-Render.setSpriteAttribute(Render.getRenderSpriteVariable(), RCSpriteAttribute.ZVelocity, 3)
+let mySprite = sprites.create(assets.image`joel`, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+stabber = sprites.create(assets.image`stabby`, SpriteKind.Enemy)
