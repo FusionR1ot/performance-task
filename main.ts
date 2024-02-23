bec48270888f48ccdaa1d743142bfadb55f9fd18
FusionR1ot <156235870+FusionR1ot@users.noreply.github.com>
2024-02-23 12:26:29 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-let joel = sprites.create(assets.image`joel`, SpriteKind.Player)
+function position (stabber: Sprite) {
+    stabber.setKind(SpriteKind.Enemy)
+}
+game.splash("\"you can't do anything\"")
+game.splash("\"you're worthless\"")
+let mySprite = Render.getRenderSpriteVariable()
+game.splash("I'll prove them wrong ")
 tiles.setCurrentTilemap(tilemap`level`)
-joel.setVelocity(50, 50)
+let stabber = sprites.create(assets.image`stabby`, SpriteKind.Enemy)
+Render.setSpriteAttribute(Render.getRenderSpriteVariable(), RCSpriteAttribute.ZVelocity, 3)
